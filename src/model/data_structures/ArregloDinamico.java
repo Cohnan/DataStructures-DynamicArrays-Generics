fb601fa3adf23e21040fab25f3290e570fc2da5c
Cohnan <sc.puerto10@uniandes.edu.co>
2019-01-25 23:21:35 -0500

    Trabaje por error en master la implementacion de generics. Regrese al ultimo commit sin estos cambios y agregue la correccion de errores y comentarios: no mensaje informativo, caso especial de borrar todos los datos, pequena modificacion a darElemento luego de encontrar documentacion

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -1,6 +1,5 @@
 package model.data_structures;
 //import java.io.*;
-import java.util.*;
 
 
 /**
@@ -57,52 +56,72 @@ public class ArregloDinamico implements IArregloDinamico {
 		}
 
 		public String darElemento(int i) {
+			if (i >= tamanoAct) return null;
 			return elementos[i];
 		}
 
 		public String buscar(String dato) {
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
 			
-			String message = "Esta en la posicion:";
+			//String message = "Esta en la posicion:";
 			Boolean encontrado = false;
-	
+			
+			// Los busca todos. Innecesario para lo pedido en la documentacion. Bastaria reemplazar
+			// encontrado = true por return true. Util para mensaje informativo
 			for (int i = 0; i < tamanoAct; i++) {
 				if (dato.compareTo(elementos[i]) == 0) {
-					message += " " + (i+1) + ",";
+					//message += " " + (i+1) + ",";
 					encontrado = true;
 				}
 			}
 			
 			//message = message.substring(0, message.length() - 1) + "."; //Regresa un mensaje infomativo
-			message = dato;
-			if (encontrado) return message;
+			//message = dato;
+			if (encontrado) return dato;
 			return null;
 		}
 
 		public String eliminar(String dato) {
 			
-			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
+			// Contendra los datos diferentes de dato compactados
 			String[] temp = new String[tamanoMax];
 			
+			// Indica si se elimino algun dato del arreglo. Podria usar buscar(), pero es innecesario
 			boolean encontrado = false;
+			//Indicara el tamano del nuevo array
 			int tempTamanoAct = 0;
 			
+			// Agrega datos de forma compacta a temp
 			int j = 0; // Indice para el nuevo array
 			for (int i = 0; i < tamanoAct; i++) {
 				if (dato.compareTo(elementos[i]) != 0){
-					encontrado = true;
 					temp[j] = elementos[i];
 					j++;
 					tempTamanoAct++;
+				} else {
+					encontrado = true;
 				}
 			}
 			
+			// Previene calculos innecesarios si no se eliminan datos
 			if (!encontrado) return null;
 			
+			// Calculos para reducir el tamano total del array en caso de que se borren muchos datos.
 			int nEliminados = tamanoAct - tempTamanoAct;
+			
+			// Caso especial: se borran todos los elementos
+			if (nEliminados == tamanoAct) {
+				elementos = new String[1];
+				tamanoAct = 0;
+				tamanoMax = 1;
+				return dato;
+			}
+			
+			// Revalua tamanoAct y el tamanoMax necesario para elementos
 			tamanoAct = tempTamanoAct;
 			while (tempTamanoAct <= tamanoMax/2) tamanoMax = tamanoMax/2;
 			
+			// Crea elementos de manera compacta con elementos restantes y tamano apropiado
 			elementos = new String[tamanoMax];
 			for (int i = 0; i < tamanoAct; i++) {
 				elementos[i] = temp[i];
